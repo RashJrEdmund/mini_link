ce43b899bd20ea58e3a7835d07baf617724b4df9
rash <orashusedmund@gmail.com>
2023-09-06 22:55:20 +0100

    changed app name to mini-link and updated README

---
+++ README.md
@@ -0,0 +1,4 @@
+### Author
+    Rash E.J
+
+## mini_link preview url: visit [deployment](minilink-dev.vercel.app)
--- src/store/store.ts
+++ src/store/store.ts
@@ -9,7 +9,7 @@ const theme_modes: THEME_MODE = "dark" // default-theme-mode
 /* ========== READEABBLE_ITEMS ========== */
 export const COLOR_PALETTE_STORE = readable(COLOR_PALETTE);
 
-export const APP_NAME = readable("ShortLink");
+export const APP_NAME = readable("mini link");
 
 /* ========== WRITABLE_ITEMS ========== */
 export const THEME = writable(theme_modes);
