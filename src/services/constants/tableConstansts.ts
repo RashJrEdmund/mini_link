beb19a4d3df28769cb54eee4d847f0f76b6ba41c
rash <orashusedmund@gmail.com>
2023-11-06 17:53:05 +0100

    updated the links per page to the public env

--- src/services/constants/tableConstansts.ts
+++ src/services/constants/tableConstansts.ts
@@ -1,3 +1,5 @@
+import { PUBLIC_LINKS_PER_PAGE } from "$env/static/public";
+
 type N = number;
 
-export const LINKS_PER_PAGE: N = 5;
+export const LINKS_PER_PAGE: N = Number(PUBLIC_LINKS_PER_PAGE) || 5;
